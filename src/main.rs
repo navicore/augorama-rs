662940f6373945a7af655b7c7a396730c48ecb89
Ed Sweeney <ed@onextent.com>
2019-06-16 18:10:10 -0700

    run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+fn index(info: web::Path<(String, u32)>) -> impl Responder {
+    format!("Hello {}! id:{}", info.0, info.1)
+}
+
+fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(web::resource("/{name}/{id}/index.html").to(index)))
+        .bind("127.0.0.1:8080")?
+        .run()
 }

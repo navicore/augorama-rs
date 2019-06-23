8479aea12eda81ccdf6db3dc0179199c16eb1101
Ed Sweeney <ed@onextent.com>
2019-06-23 16:19:28 -0700

    logs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 use actix_web::{web, App, HttpServer, Responder};
+//use log::Level;
 
 fn index(info: web::Path<(String, u32)>) -> impl Responder {
+    debug!("ejs this is a debug {}", "message");
     format!("Hello {}! id:{}", info.0, info.1)
 }
 
 fn main() -> std::io::Result<()> {
+    env_logger::init();
     HttpServer::new(|| App::new().service(web::resource("/{name}/{id}/hello").to(index)))
         .bind("127.0.0.1:8080")?
         .run()

7a73e40e528521f8920a8c8a77e95fe641d8be21
Ed Sweeney <ed@onextent.com>
2019-09-27 10:29:36 -0700

    integration test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,70 @@
 extern crate env_logger;
 extern crate log;
 
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use log::{debug, info};
+use riker::actors::*;
+use riker::system::ActorSystem;
+use warp::{self, Filter, path};
+
+use crate::au::actor::AugieActor;
+use crate::au::msg::AuCmd::Get;
+use crate::au::msg::AuMsg;
+
 pub mod au;
+
+pub fn serve() {
+    env_logger::init();
+    info!("starting actor space");
+
+    let sys = Arc::new(Mutex::new(ActorSystem::new().unwrap()));
+    let sys_shared = sys.clone();
+
+    let roots: Arc<Mutex<HashMap<String, ActorRef<AuMsg<String>>>>> =
+        Arc::new(Mutex::new(HashMap::new()));
+    let roots_shared: Arc<Mutex<HashMap<String, ActorRef<AuMsg<String>>>>> = roots.clone();
+
+    let actor1_level = path!("actor" / String / String).map(move |typ: String, id| -> String {
+        let sys_shared = sys_shared.lock().unwrap();
+
+        let msg = AuMsg {
+            msg: "haha".to_string(),
+            cmd: Get,
+            forward: None,
+        };
+
+        // Check for a specific one.
+        let mut roots_shared = roots_shared.lock().unwrap();
+        let actor = match roots_shared.get(&typ) {
+            Some(actor) => actor.clone(),
+            None => {
+                debug!("creating root actor of type {}", typ);
+                let props = AugieActor::props();
+                let new_actor = sys_shared.actor_of(props, &typ).unwrap();
+                roots_shared.insert(typ.to_string(), new_actor.clone());
+                new_actor
+            }
+        };
+
+        actor.tell(msg, None);
+
+        //demo::main();
+        format!("Hello {} {}!", typ, id)
+    });
+
+    let actor2_level = path!("actor" / String / String / String / String).map(
+        |parent, parent_id, typ, id| -> String {
+            debug!(
+                "handling parent type {} id {} type {} id {} ",
+                parent, parent_id, typ, id
+            );
+            //augieactor::main();
+            format!("Hello {}'s {} {}!", parent_id, typ, id)
+        },
+    );
+
+    let routes = actor2_level.or(actor1_level);
+    warp::serve(routes).run(([127, 0, 0, 1], 3030));
+}
--- tests/actor_test.rs
+++ tests/actor_test.rs
@@ -1,10 +1,22 @@
 extern crate augorama;
 
-use augorama::au::msg::AuTelemetry;
+use std::{thread, time};
 
 #[test]
 fn actor_messaging_works() {
-    let t = AuTelemetry::default();
-    assert_eq!(t.name, "measurement".to_string());
-    assert_eq!(t.value, 0.0);
+    thread::spawn(move || {
+        augorama::serve()
+    });
+    let two_seconds = time::Duration::from_millis(1000);
+    thread::sleep(two_seconds);
+
+    match reqwest::get("http://localhost:3030/actor/person/Mary") {
+        Ok(mut result) => {
+            match result.text() {
+                Ok(t) => assert_eq!(t, "Hello person Mary!"),
+                Err(_) => assert!(false)
+            }
+        }
+        Err(_) => assert!(false)
+    }
 }

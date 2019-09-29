d3cb8bdfa4af3a5d865425520d182ba2c7aefc52
Ed Sweeney <ed@onextent.com>
2019-09-29 10:14:10 -0700

    broken icon

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+#![doc(
+    html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
+    html_favicon_url = "https://www.rust-lang.org/favicon.ico",
+    html_root_url = "https://doc.rust-lang.org/"
+)]
 extern crate augorama;
 extern crate env_logger;
 extern crate log;
 
 use augorama::serve;
 
-///
 /// entry point to start the server
 ///
 fn main() {

25d35a7c42c4814b16ff58021b909eb56c217732
Ed Sweeney <ed@onextent.com>
2019-10-05 14:19:58 -0700

    dry

--- src/lib.rs
+++ src/lib.rs
@@ -23,25 +23,59 @@
 extern crate env_logger;
 extern crate log;
 
+use std::borrow::Borrow;
+use std::collections::hash_map::RandomState;
 use std::collections::HashMap;
+use std::ops::Deref;
 use std::sync::{Arc, Mutex, MutexGuard};
 
+use futures::executor::block_on;
+use futures::future::RemoteHandle;
 use log::{debug, info};
 use riker::actors::*;
 use riker::system::ActorSystem;
 use riker_patterns::ask::*;
-use warp::{self, path, Filter};
-
-use futures::future::RemoteHandle;
+use warp::{self, Filter, path};
 
 use crate::au::actor::AugieActor;
 use crate::au::msg::AuCmd::Get;
 use crate::au::msg::AuMsg;
-use futures::executor::block_on;
-use std::borrow::Borrow;
-use std::ops::Deref;
 
 pub mod au;
+
+fn create_actor_and_wait(typ: String, id: String, path: Vec<String>,
+                         sys_shared: MutexGuard<ActorSystem>,
+                         mut roots_shared: MutexGuard<HashMap<String, ActorRef<AuMsg<String>>, RandomState>>) -> String {
+    let msg: AuMsg<String> = AuMsg {
+        msg: id.clone(),
+        cmd: Get,
+        path,
+    };
+
+    let actor = match roots_shared.get(&typ) {
+        Some(actor) => {
+            debug!("found existing root actor of type {}", typ);
+            actor.clone()
+        }
+        None => {
+            debug!("creating root actor of type {}", typ);
+            let props = AugieActor::props();
+            let new_actor = sys_shared.actor_of(props, &typ).unwrap();
+            roots_shared.insert(typ.to_string(), new_actor.clone());
+            new_actor
+        }
+    };
+
+    let sys = sys_shared.borrow().deref();
+    let res: RemoteHandle<AuMsg<String>> = ask(sys, &actor, msg);
+    let response = block_on(res);
+
+    //ejs todo result in json:
+    //ejs todo result in json:
+    //ejs todo result in json:
+    //ejs todo result in json:
+    format!("Hi {} {}!", typ, response.msg)
+}
 
 /// blocking call to run server.  server will open a port and expect http requests.
 pub fn serve() {
@@ -51,80 +85,50 @@ pub fn serve() {
     let sys = Arc::new(Mutex::new(ActorSystem::new().unwrap()));
     let sys_shared1 = sys.clone();
     let sys_shared2 = sys.clone();
-    //let sys_shared2 = sys.clone();
+    let sys_shared3 = sys.clone();
+    let sys_shared4 = sys.clone();
+    let sys_shared5 = sys.clone();
 
     let roots: Arc<Mutex<HashMap<String, ActorRef<AuMsg<String>>>>> =
         Arc::new(Mutex::new(HashMap::new()));
+
     let roots_shared1: Arc<Mutex<HashMap<String, ActorRef<AuMsg<String>>>>> = roots.clone();
     let roots_shared2: Arc<Mutex<HashMap<String, ActorRef<AuMsg<String>>>>> = roots.clone();
+    let roots_shared3: Arc<Mutex<HashMap<String, ActorRef<AuMsg<String>>>>> = roots.clone();
+    let roots_shared4: Arc<Mutex<HashMap<String, ActorRef<AuMsg<String>>>>> = roots.clone();
+    let roots_shared5: Arc<Mutex<HashMap<String, ActorRef<AuMsg<String>>>>> = roots.clone();
 
     // route for root level actors, ie: an actor type and an instance id
-    let actor1_level =
-        path!("actor" / String / String).map(move |typ: String, id: String| -> String {
-            let sys_shared: MutexGuard<ActorSystem> = sys_shared1.lock().unwrap();
-            let msg: AuMsg<String> = AuMsg {
-                msg: id.clone(),
-                cmd: Get,
-                path: vec![id.clone()],
-            };
-
-            // Check for a specific one.
-            let mut roots_shared = roots_shared1.lock().unwrap();
-            let actor = match roots_shared.get(&typ) {
-                Some(actor) => actor.clone(),
-                None => {
-                    debug!("creating root actor of type {}", typ);
-                    let props = AugieActor::props();
-                    let new_actor = sys_shared.actor_of(props, &typ).unwrap();
-                    roots_shared.insert(typ.to_string(), new_actor.clone());
-                    new_actor
-                }
-            };
-
-            let sys = sys_shared.borrow().deref();
-            let res: RemoteHandle<AuMsg<String>> = ask(sys, &actor, msg);
-            let response = block_on(res);
-
-            //ejs todo result in json:
-            format!("Hi {} {}!", typ, response.msg)
+    let route2 =
+        path!("actor" / String / String).map(move |root_typ: String, id: String| -> String {
+            create_actor_and_wait(root_typ, id.clone(), vec!(id), sys_shared1.lock().unwrap(), roots_shared1.lock().unwrap())
         });
 
     // 2nd level actors, ie: an actor type and an instance id that is the child of a root actor.
-    let actor2_level = path!("actor" / String / String / String / String).map(
-        move |parent: String, parent_id: String, typ: String, id: String| -> String {
-            let sys_shared: MutexGuard<ActorSystem> = sys_shared2.lock().unwrap();
-
-            let msg: AuMsg<String> = AuMsg {
-                msg: id.clone(),
-                cmd: Get,
-                path: vec![parent_id.clone(), typ.clone(), id.clone()],
-            };
-
-            // Check for a specific one.
-            let mut roots_shared = roots_shared2.lock().unwrap();
-            let actor = match roots_shared.get(&parent) {
-                Some(actor) => {
-                    debug!("found existing parent actor of type {}", parent);
-                    actor.clone()
-                }
-                None => {
-                    debug!("creating parent actor of type {}", parent);
-                    let props = AugieActor::props();
-                    let new_actor = sys_shared.actor_of(props, &parent).unwrap();
-                    roots_shared.insert(parent.to_string(), new_actor.clone());
-                    new_actor
-                }
-            };
-
-            let sys = sys_shared.borrow().deref();
-            let res: RemoteHandle<AuMsg<String>> = ask(sys, &actor, msg);
-            let response = block_on(res);
-
-            //ejs todo result in json:
-            format!("Hello {}'s {} {}!", parent_id, typ, response.msg)
+    let route4 = path!("actor" / String / String / String / String).map(
+        move |root_typ: String, root_id: String, child_typ: String, id: String| -> String {
+            create_actor_and_wait(root_typ, id.clone(), vec!(root_id.clone(), child_typ.clone(), id.clone()), sys_shared2.lock().unwrap(), roots_shared2.lock().unwrap())
         },
     );
 
-    let routes = actor2_level.or(actor1_level);
+    let route6 = path!("actor" / String / String / String / String / String / String).map(
+        move |root_typ: String, root_id: String, child1_typ: String, child1_id: String, child_typ: String, id: String| -> String {
+            create_actor_and_wait(root_typ, id.clone(), vec!(root_id.clone(), child1_typ.clone(), child1_id.clone(), child_typ.clone(), id.clone()), sys_shared3.lock().unwrap(), roots_shared3.lock().unwrap())
+        },
+    );
+
+    let route8 = path!("actor" / String / String / String / String / String / String / String / String).map(
+        move |root_typ: String, root_id: String, child1_typ: String, child1_id: String, child2_typ: String, child2_id: String, child_typ: String, id: String| -> String {
+            create_actor_and_wait(root_typ, id.clone(), vec!(root_id.clone(), child1_typ.clone(), child1_id.clone(), child2_typ.clone(), child2_id.clone(), child_typ.clone(), id.clone()), sys_shared4.lock().unwrap(), roots_shared4.lock().unwrap())
+        },
+    );
+
+    let route10 = path!("actor" / String / String / String / String / String / String / String / String / String / String).map(
+        move |root_typ: String, root_id: String, child1_typ: String, child1_id: String, child2_typ: String, child2_id: String, child3_typ: String, child3_id: String, child_typ: String, id: String| -> String {
+            create_actor_and_wait(root_typ, id.clone(), vec!(root_id.clone(), child1_typ.clone(), child1_id.clone(), child2_typ.clone(), child2_id.clone(), child3_typ.clone(), child3_id.clone(), child_typ.clone(), id.clone()), sys_shared5.lock().unwrap(), roots_shared5.lock().unwrap())
+        },
+    );
+
+    let routes = route10.or(route8.or(route6.or(route4.or(route2))));
     warp::serve(routes).run(([127, 0, 0, 1], 3030));
 }

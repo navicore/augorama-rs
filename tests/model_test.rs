7d43b8b1277ab9dee155505eb35bf83a5b9d7e40
Ed Sweeney <ed@onextent.com>
2021-11-12 19:09:47 -0800

    test

---
+++ tests/model_test.rs
@@ -0,0 +1,18 @@
+//! # Test Actor Props Lifecycle
+//!
+extern crate augorama;
+
+#[test]
+fn actor_has_props() {
+    // thread::spawn(move || augorama::serve());
+    // let two_seconds = time::Duration::from_millis(1000);
+    // thread::sleep(two_seconds);
+    //
+    // match reqwest::get("http://localhost:3030/actor/person/Mary") {
+    //     Ok(mut result) => match result.text() {
+    //         Ok(t) => assert_eq!(t, "[]"),
+    //         Err(_) => assert!(false),
+    //     },
+    //     Err(_) => assert!(false),
+    // }
+}
